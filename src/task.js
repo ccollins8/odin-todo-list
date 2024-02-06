67a0cde43b7934b2ae8a8d3f261667ea6dd9ec7e
coreycollins5 <coreyftw21@gmail.com>
2024-02-05 19:19:38 -0700

    Add basic layout

---
+++ src/task.js
@@ -0,0 +1,44 @@
+class Task {
+    constructor(title, description, dueDate, priorty) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priorty = priorty;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    setTitle(title) {
+        this.title = title;
+    } 
+
+    getDescription() {
+        return this.description;
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    getDueDate() {
+        return this.dueDate;
+    }
+
+    setDueDate(dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    getPrioity() {
+        return this.priorty;
+    }
+
+    setTitle(priorty) {
+        this.priorty = priorty;
+    }
+
+
+
+
+}
